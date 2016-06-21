24d2ddc1b7e96003a1fae97de75af7285aa2fb36
Ray Myers <raydog87@gmail.com>
2016-06-21 00:57:38 -0700

    A bunch more test cases

--- test/advent350_test.js
+++ test/advent350_test.js
@@ -27,7 +27,7 @@ describe("Adventure 350", function () {
     });
   });
 
-  describe("basic command handling", function () {
+  describe("basic game mechanics", function () {
     beforeEach(function () {
       _test("", /welcome to adventure/i);
       _test("no", /standing at the end of a road/i);
@@ -72,6 +72,10 @@ describe("Adventure 350", function () {
       _test("inventory", /not carrying anything/i);
     });
 
+    it("can drink from the stream", function () {
+      _test("drink", /taken a drink/i);
+    });
+
     it("has flavor text for odd stuff", function () {
       _test("calm keys", /no keys here/i);
       _test("building", /inside a building/i);
@@ -84,12 +88,161 @@ describe("Adventure 350", function () {
       _test("suspend food", /don't understand/i);
       _test("hours lamp", /don't understand/i);
       _test("log lamp", /don't understand/i);
+      _test("rub lamp", /not particularly rewarding/i);
+      _test("rub keys", /nothing unexpected happens/i);
       _test("log", /intransitive verb not implemented/i);
+      _test("nothing", /ok/i);
+      _test("nothing keys", /ok/i);
+      _test("eat bird", /no bird here/i);
+      _test("wave", /wave what/);
+      _test("wave lamp", /aren't carrying it/i);
+      _test("eat food", /it was delicious/i);
+      _test("inventory bottle", /right here with you/i);
+
+      _test("take lamp", /ok/i);
+      _test("inventory", /holding the following/i, /brass lantern/i);
+      _test("wave lamp", /nothing happens/i);
+      _test("drop lantern", /ok/i);
     });
 
     it("can calculate scores at start", function () {
       _test("score", /showing up: 2/i, /score: 2/i);
     });
+
+    it("supports xyzzy magic", function () {
+      _test("xyzzy", /nothing happens/i);
+      _test("building", /inside a building/i);
+      _test("take lamp", /ok/i);
+      _test("lamp on", /lamp is now on/i);
+      _test("xyzzy", /debris room filled with stuff/i);
+    });
+
+    it("supports plugh magic", function () {
+      _test("plugh", /nothing happens/i);
+      _test("building", /inside a building/i);
+      _test("take lamp", /ok/i);
+      _test("on lamp", /lamp is now on/i);
+      _test("plugh", /large "Y2" on a rock/i);
+    });
+
+    it("understands darkness", function () {
+      _test("building", /inside a building/i);
+      _test("xyzzy", /it is now pitch dark/i);
+    });
+
+    it("can toggle the lamp on/off", function () {
+      _test("building", /inside a building/i);
+      _test("take lamp", /ok/i);
+      _test("plugh", /pitch dark/i);
+      _test("lamp on", /lamp is now on/i, /large "Y2" on a rock/);
+      _test("lamp off", /lamp is now off/i);
+      _test("look", /pitch dark/i);
+    });
+  });
+
+  describe("handles scenarios", function () {
+
+    beforeEach(function () {
+      _test("", /welcome to adventure/i);
+      _test("nope", /standing at the end of a road/i);
+    });
+
+    it("with the bird", function () {
+      _test("building", /inside a building/i);
+      _test("take lamp", /ok/i);
+      _test("on lamp", /lamp is now on/i);
+      _test("xyzzy", /debris room filled with stuff/i);
+      _test("take rod", /ok/);
+      _test("w", /east\/west canyon/i);
+      _test("w", /splendid chamber/i, /little bird/i);
+      _test("take bird", /becomes disturbed/i, /you cannot catch it/i);
+      _test("eat bird", /lost my appetite/i);
+      _test("calm bird", /care to explain how/);
+      _test("kill bird", /little bird is now dead/i);
+    });
+
+    it("with the snake", function () {
+      _test("building", /inside a building/i);
+      _test("take lamp", /ok/i);
+      _test("on lamp", /lamp is now on/i);
+      _test("xyzzy", /debris room filled with stuff/i);
+      _test("e", /crawling over cobbles/i, /small wicker cage/i);
+      _test("take cage", /ok/i);
+      _test("w", /debris room/i);
+      _test("w", /east\/west canyon/i);
+      _test("w", /splendid chamber/i, /little bird/i);
+      _test("take bird", /ok/i);
+      _test("w", /small pit breathing traces of white mist/i, /stone steps lead down/i);
+      _test("down", /vast hall/i, /stone steps lead up/i);
+      _test("north", /hall of the mountain king/i, /fierce snake/i);
+      _test("calm snake", /care to explain how/i);
+      _test("attack snake", /doesn't work/i);
+      _test("kill", /doesn't work/i);
+      _test("eat snake", /just lost my appetite/i);
+      _test("feed snake", /snake has now devoured your bird/i);
+    });
+  });
+
+  describe("can progress in the game", function () {
+
+    beforeEach(function () {
+      _test("", /welcome to adventure/i);
+      _test("nah", /standing at the end of a road/i);
+    });
+
+    it("can unlock the grate", _upTo(1));
+    it("can go down the pit", _upTo(2));
+    it("can bring back diamonds", _upTo(3));
+
+
+    function _upTo(num) {
+      return () => STEPS.slice(0, num).forEach(fn => fn());
+    }
+
+    var STEPS = [
+      function () {
+        _test("building", /inside a building/i);
+        _test("take lamp", /ok/i);
+        _test("take keys", /ok/i);
+        _test("exit", /end of road again/i);
+        _test("look", /not allowed to give more detail/i, /end of a road/i);
+        _test("gully", /valley in the forest/i);
+        _test("downstream", /2-inch slit in the rock/i);
+        _test("downstream", /grate mounted in concrete/i);
+        _test("unlock grate", /now unlocked/i);
+        _test("enter", /small chamber beneath a 3x3 steel grate/i);
+      },
+      function () {
+        _test("w", /crawling over cobbles/i, /small wicker cage/i);
+        _test("take cage", /ok/i);
+        _test("lamp on", /lamp is now on/);
+        _test("w", /debris room/i, /xyzzy/i, /black rod/i);
+        _test("west", /awkward sloping east\/west canyon/i);
+        _test("w", /frozen rivers of orange stone/i, /little bird/i);
+        _test("take bird", /ok/i);
+        _test("east", /awkward sloping east\/west canyon/i);
+        _test("e", /in debris room/i, /black rod/i);
+        _test("take rod", /ok/i);
+        _test("w", /awkward sloping east\/west canyon/i);
+        _test("w", /bird chamber/i);
+        _test("w", /small pit breathing traces of white mist/i, /stone steps lead down/i);
+        _test("down", /vast hall/i, /stone steps lead up/i);
+      },
+      function () {
+        _test("w", /bank of a fissure/i);
+        _test("wave rod", /crystal bridge/i, /spans the fissure/i);
+        _test("cross", /west side/i, /spans the fissure/i, /diamonds/i);
+        _test("take diamonds", /ok/i);
+        _test("east", /east bank/i);
+        _test("e", /hall of mists/i);
+        _test("up", /top of small pit/i);
+        _test("e", /bird chamber/i);
+        _test("e", /east\/west canyon/i);
+        _test("east", /debris room/i);
+        _test("xyzzy", /inside building/i);
+        _test("drop diamonds", /ok/i);
+      }
+    ];
   });
 
   /**
